c139b4fca23e489b34dce9c49c15e93b25609257
Sanket Mosamkar <sanketmosamkar573@gmail.com>
2025-12-15 17:12:44 +0530

    Changes git repo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
+
 const Home = () => {
   let [electronics, setElectronics] = useState([]);
   let [clothes, setClothes] = useState([]);
