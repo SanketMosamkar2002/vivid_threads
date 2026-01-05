271eb596d2e65cff467b61769f6593eec09947a1
Sanket Mosamkar <sanketmosamkar573@gmail.com>
2026-01-05 18:33:24 +0530

    removed origin part

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 dotenv.config();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 const url = process.env.MONGODBURI;
 mongoose
   .connect(url)
